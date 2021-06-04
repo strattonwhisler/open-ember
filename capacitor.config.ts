54fd6b41b67cb493e5bd983e3e5e8b68132e44b4
stratton <stratton@NEPTUNE.localdomain>
2021-06-03 20:46:44 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'open-ember',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
