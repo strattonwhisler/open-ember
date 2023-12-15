8a6d3116283e678b72e4f962fdbb8278e188e2e3
Stratton Whisler <stratton.whisler@briebug.com>
2023-12-15 12:19:01 -0800

    improve device preview layout.

--- src/app/home/device-preview.component.ts
+++ src/app/home/device-preview.component.ts
@@ -5,6 +5,11 @@ import { Device } from '~domain/device';
 import { BatteryComponent } from '~shared/battery.component';
 import { ColorDotComponent } from '~shared/color.component';
 import { TemperatureComponent } from '~shared/temperature.component';
+import { DevicePowerStatePipe } from '~shared/device-power-state.pipe';
+import { NgSwitch, NgSwitchCase, PercentPipe } from '@angular/common';
+import { DevicePowerState } from '~shared/device-power-state';
+import { RgbaPipe } from '~shared/color.pipe';
+import { TemperaturePipe } from '~shared/temperature.pipe';
 
 
 @Component({
@@ -22,22 +27,80 @@ import { TemperatureComponent } from '~shared/temperature.component';
         <ion-grid>
           <ion-row class="ion-justify-content-between">
             <ion-col>
-              <oe-temperature [temperature]="device.currentTemperature" text="Current"></oe-temperature>
+<!--              <oe-temperature [temperature]="device.currentTemperature" text="Current"></oe-temperature>-->
+              <div class="flex ion-justify-content-center">
+                  <p class="ion-text-center">
+              <ion-icon name="thermometer-outline"></ion-icon>
+              <br/>
+              <ion-text color="medium">
+                <small class="text-mute">Current</small>
+              </ion-text>
+                  </p>
+              </div>
             </ion-col>
             <ion-col>
-              <oe-temperature [temperature]="device.targetTemperature" text="Target"></oe-temperature>
+<!--              <oe-temperature [temperature]="device.targetTemperature" text="Target"></oe-temperature>-->
+              <div class="flex ion-justify-content-center">
+                  <p class="ion-text-center">
+              <ion-icon name="thermometer-outline"></ion-icon>
+              <br/>
+              <ion-text color="medium">
+                <small class="text-mute">Target</small>
+              </ion-text>
+                  </p>
+              </div>
             </ion-col>
             <ion-col>
-              <oe-battery [battery]="device.battery"></oe-battery>
+<!--              <oe-battery [battery]="device.battery"></oe-battery>-->
+              <div class="flex ion-justify-content-center">
+              <ng-container [ngSwitch]="device.battery | devicePowerState">
+                <ion-icon *ngSwitchCase="DevicePowerState.Charging" name="battery-charging-outline"></ion-icon>
+                <ion-icon *ngSwitchCase="DevicePowerState.Dead" name="battery-dead-outline"></ion-icon>
+                <ion-icon *ngSwitchCase="DevicePowerState.Half" name="battery-half-outline"></ion-icon>
+                <ion-icon *ngSwitchCase="DevicePowerState.Full" name="battery-full-outline"></ion-icon>
+              </ng-container>
+              </div>
             </ion-col>
             <ion-col>
-              <oe-color-dot [color]="device.color"></oe-color-dot>
+<!--              <oe-color-dot [color]="device.color"></oe-color-dot>-->
+              <div class="flex ion-justify-content-center">
+              <ion-icon name="color-filter-outline"></ion-icon>
+              </div>
+            </ion-col>
+          </ion-row>
+          <ion-row class="ion-justify-content-between">
+            <ion-col>
+              <div class="flex ion-justify-content-center">
+                <ion-text>{{ device.currentTemperature | temperature }}</ion-text>
+              </div>
+            </ion-col>
+            <ion-col>
+              <div class="flex ion-justify-content-center">
+                <ion-text>{{ device.targetTemperature | temperature }}</ion-text>
+              </div>
+            </ion-col>
+            <ion-col>
+              <div class="flex ion-justify-content-center">
+                <ion-text>{{ device.battery | percent }}</ion-text>
+              </div>
+            </ion-col>
+            <ion-col>
+              <div class="flex ion-justify-content-center">
+                <svg style="height: 1em;" viewBox="0 0 1 1" xmlns="http://www.w3.org/2000/svg">
+                  <circle r="0.5" cx="0.5" cy="0.5" [style.fill]="device.color | rgba"/>
+                </svg>
+              </div>
             </ion-col>
           </ion-row>
         </ion-grid>
       </ion-card-content>
     </ion-card>
   `,
+  styles: [`
+    .flex {
+      display: flex;
+    }
+  `],
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
   imports: [
@@ -45,9 +108,17 @@ import { TemperatureComponent } from '~shared/temperature.component';
     IonicModule,
     TemperatureComponent,
     BatteryComponent,
-    ColorDotComponent
+    ColorDotComponent,
+    DevicePowerStatePipe,
+    NgSwitchCase,
+    PercentPipe,
+    NgSwitch,
+    RgbaPipe,
+    TemperaturePipe
   ]
 })
 export class DevicePreviewComponent {
+  readonly DevicePowerState = DevicePowerState;
+
   @Input() device: Device;
 }
